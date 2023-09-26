0bec7642872699a5b9339e9b155e700cfca2bb51
hasanoqool <hasanoqool98@gmail.com>
2023-09-26 15:16:34 +0300

    Fine-tuning a network using tensorflow

--- fine_tune_tf.py
+++ fine_tune_tf.py
@@ -9,3 +9,57 @@ from keras.layers import *
 from keras.optimizers import RMSprop
 from keras.preprocessing.image import *
 from tensorflow_hub import KerasLayer
+
+
+SEED = 999
+
+
+def build_network(base_model, classes):
+    return Sequential([base_model,Dense(classes),Softmax()])
+
+
+def load_images_and_labels(image_paths,target_size=(256, 256)):
+    images = []
+    labels = []
+
+    for image_path in image_paths:
+        image = load_img(image_path,
+        target_size=target_size)
+        image = img_to_array(image)
+        label = image_path.split(os.path.sep)[-2]
+        images.append(image)
+        labels.append(label)
+    return np.array(images), np.array(labels)
+
+
+def main():
+    dataset_path = pathlib.Path("/content/drive/MyDrive/Colab Notebooks/Computer Vision Repos/datasets/flowers17")
+    files_pattern = (dataset_path / 'images' / '*' / '*.jpg')
+    image_paths = [*glob(str(files_pattern))]
+    CLASSES = {p.split(os.path.sep)[-2] for p in image_paths}
+
+    X, y = load_images_and_labels(image_paths)
+    X = X.astype('float') / 255.0
+    y = LabelBinarizer().fit_transform(y)
+    (X_train, X_test,y_train, y_test) = train_test_split(X, y,test_size=0.2,random_state=SEED)
+
+    model_url = ('https://tfhub.dev/google/imagenet/resnet_v1_152/feature_vector/4')
+    base_model = KerasLayer(model_url, input_shape=(256,256, 3))
+    base_model.trainable = False
+
+    model = build_network(base_model, len(CLASSES))
+
+    BATCH_SIZE = 32
+    augmenter = ImageDataGenerator(rotation_range=30,horizontal_flip=True,width_shift_range=0.1,
+    height_shift_range=0.1,shear_range=0.2,zoom_range=0.2,fill_mode='nearest')
+    train_generator = augmenter.flow(X_train, y_train,BATCH_SIZE)
+
+    EPOCHS = 20
+    model.compile(loss='categorical_crossentropy',optimizer=RMSprop(lr=1e-3),metrics=['accuracy'])
+    model.fit(train_generator,steps_per_epoch=len(X_train) // BATCH_SIZE,validation_data=(X_test, y_test),epochs=EPOCHS)
+    result = model.evaluate(X_test, y_test)
+    
+    print(f'Test accuracy: {result[1]}')
+
+if __name__=='__main__':
+    main()
